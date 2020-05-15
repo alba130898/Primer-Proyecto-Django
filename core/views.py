ab5e9fd5bc0b672890474be59d807678976e3138
Alba Cuatecontzi <isa081398@gmail.com>
2020-05-15 09:33:36 -0500

    commit web personal

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me">Acerca de</a></li>
+
+"""
+
+# Create your views here.
+def home(request):
+    return HttpResponse(html_base + """
+    <h2>Portada</h2>
+    <p>Esto es la portada.</>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+    <h2>Acerca de</h2>
+    <p>Me llamo Alba y soy programador.</p>
+    """)
