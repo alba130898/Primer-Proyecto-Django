2912c3a60588dfb8c4641bf0225d93074c2d16ec
Alba Cuatecontzi <isa081398@gmail.com>
2020-05-15 20:24:50 -0500

    commit web personal

--- core/views.py
+++ core/views.py
@@ -1,36 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me">Acerca de</a></li>
-    <li><a href="/portfolio">Portafolio</a></li>
-    <li><a href="/contact">Contacto</a></li>
-
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-    <h2>Portada</h2>
-    <p>Esto es la portada.</>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</h2>
-    <p>Me llamo Alba y soy programador.</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-    <h2>Portafolio</h2>
-    <p>Algunos de mis trabajos.</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-    <h2>Contacto</h2>
-    <p>Aqui les dejo mi email para preguntarme dudas: <a href= "mailto:isa081398@gmail.com">isa081398@gmail.com</a></p>
-    """)
+    return render(request, "core/contact.html")
