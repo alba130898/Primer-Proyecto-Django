cc7eee07186fad4883b4644a358abfe089b1c6a7
Alba Cuatecontzi <isa081398@gmail.com>
2020-05-15 20:02:44 -0500

    commit web personal

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ html_base = """
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me">Acerca de</a></li>
+    <li><a href="/portfolio">Portafolio</a></li>
+    <li><a href="/contact">Contacto</a></li>
 
 """
 
@@ -19,4 +21,16 @@ def about(request):
     return HttpResponse(html_base + """
     <h2>Acerca de</h2>
     <p>Me llamo Alba y soy programador.</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+    <h2>Portafolio</h2>
+    <p>Algunos de mis trabajos.</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+    <h2>Contacto</h2>
+    <p>Aqui les dejo mi email para preguntarme dudas: <a href= "mailto:isa081398@gmail.com">isa081398@gmail.com</a></p>
     """)
